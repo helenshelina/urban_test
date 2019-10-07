41ede7bd65d36b5d4980abca396ac6e90d0cf0fa
helenshelina <helenshelina@gmail.com>
2019-10-07 20:52:38 +0300

    add gitignore, create class for storing parking cost

--- tests/test_parking_calculator.py
+++ tests/test_parking_calculator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # vim: set encoding=utf-8
-import time
 
 from pages.calculator_page import CalculatorPage
+from support.parking_cost import ParkingCost
 
 
 def test_simple_calculate_parking(browser):
@@ -13,8 +13,9 @@ def test_simple_calculate_parking(browser):
     calc_page.fill_end_date('2019-10-07')
     calc_page.choose_delay_time(0)
     calc_page.calculate()
+    parking_cost = ParkingCost()
 
-    assert "18.00" in calc_page.final_cost()
+    assert parking_cost.valet_parking_cost() in calc_page.final_cost()
     assert "1 Days, 0 Hours, 0 Minutes" in calc_page.final_cost()
 
 
